e884f1b8291d59934d0d41ba81f9683edac84c0a
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-04-03 18:45:59 +0900

    save

---
+++ 10/interface/instanceof.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'Triangle.php';
+// require_once 'Square.php';
+
+$figs = [];
+$figs[] = new Triangle(10, 5);
+$figs[] = new Triangle(20, 5);
+$figs[] = new Triangle(50, 5);
+
+foreach ($figs as $fig) {
+    if ($fig instanceof IFigure) {
+        print $fig->getArea() . " - ";
+    }
+}
